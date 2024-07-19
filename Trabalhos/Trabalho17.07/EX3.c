a06d4bdee7ab378dbcda9c184cc86f5c7f89e9ab
Thiago <124899004+umthiago@users.noreply.github.com>
2024-07-19 11:06:47 -0300

    Create EX3.c

---
+++ Trabalhos/Trabalho17.07/EX3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int nota1;
+    int nota2;
+    int nota3;
+
+    printf("informe a primeira nota ");
+    scanf("%d", &nota1);
+
+    printf("informe a segunda nota ");
+    scanf("%d", &nota2);
+
+    printf("informe a terceira nota ");
+    scanf("%d", &nota3);
+
+    int media = (nota1+ nota2 + nota3)/3;
+
+    printf("Sua media aritmetica foi: ", media);
+
+    if(media >= 7){
+        printf("ALUNO APROVADO");
+    } else{
+        printf("ALUNO REPROVADO");
+    }
+return 0;
+}
