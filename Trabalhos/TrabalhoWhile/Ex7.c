e59071bc7a97b7fed7546067e236e98b50e95d45
Thiago <124899004+umthiago@users.noreply.github.com>
2024-08-13 17:05:37 -0300

    Create Ex7.c

---
+++ Trabalhos/TrabalhoWhile/Ex7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  double joao = 0;
+  double jose = 0;
+ 
+
+
+  
+  while ( jose < ){
+    printf("%d\n", valor);
+
+    valor = valor - 5;
+    
+    
+    if(valor == 5){
+      valor = 0;
+      printf("%d\n", valor);
+    }
+    aux = valor;
+  }
+  
+  return 0;
+}
