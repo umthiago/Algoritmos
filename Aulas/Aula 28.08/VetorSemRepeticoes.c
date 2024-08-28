5cfaf1d11e18fd59a23a8e24506db9ab50a0031f
Thiago <124899004+umthiago@users.noreply.github.com>
2024-08-28 16:37:21 -0300

    Create VetorSemRepeticoes.c

---
+++ Aulas/Aula 28.08/VetorSemRepeticoes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    int vetorInteiros[num];
+    int aux = 0;
+
+    for (int i = 0; i < 10; i++){
+        printf("Digite o %dº valor: ", i+1);
+        scanf("%d", &vetorInteiros[i]);
+        
+    }
+    for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++){
+            if (aux == vetorInteiros[i]){
+                vetorInteiros[i] = 0;
+                num--;
+            }
+
+
+
+        }
+        printf("%d ", vetorInteiros[i]);
+        aux = vetorInteiros[i];
+        
+        
+        
+
+    }
+    printf("Vetor sem repetições: ");
+    for (int i = 0; i < 10; i++){
+        if (vetorInteiros[i] != 0){
+            printf("%d ", vetorInteiros[i]);
+        }
+    }
+    return 0;
+}
