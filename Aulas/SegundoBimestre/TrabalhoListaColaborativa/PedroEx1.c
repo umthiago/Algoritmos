b626453f0c7ec317d90d845824440e6a0d14207d
Thiago <124899004+umthiago@users.noreply.github.com>
2024-08-30 10:59:21 -0300

    Create PedroEx1.c

---
+++ Aulas/SegundoBimestre/TrabalhoListaColaborativa/PedroEx1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int valor = 0;
+    char nacionalidade[20];
+
+    printf("Insira o valor da sua compra: ");
+    scanf("%d", &valor);
+    printf("Insira se a compra é Internacional ou Nacional: ");
+    scanf("%c", nacionalidade);
+
+    if (nacionalidade == "Internacional" || nacionalidade == "internacional"){
+        
+    }
+
+    
+
+   
+    return 0;
+}
