e4d8e01558231ba5eb87dca53a2d3a9ab018050e
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-04 14:30:22 -0300

    Update ExLerVetorAoContrario.c

--- Aulas/Aula04.09/ExLerVetorAoContrario.c
+++ Aulas/Aula04.09/ExLerVetorAoContrario.c
@@ -5,13 +5,13 @@ int main() {
     char palavra[100];
     int contador = -1;
 
-    printf("Digite uma palavra: ");
+    printf("Digite uma palavra: \n");
     scanf("%s", palavra);
 
     for (int i = 0; i < strlen(palavra); i++){
         contador++;
     }
-    
+    printf("A palavra ao contrario fica: ");
     for(int i = contador; i < strlen(palavra); i--){
         printf("%c", palavra[i]);
     }
