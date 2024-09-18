6c553b592b0a469d6bf3f27de1937da616081728
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-18 14:55:17 -0300

    Update EX1.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX1.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX1.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Cliente {
-  int id;
-  char nome[50]; 
-  char email[50];
-  int idade;
+struct Evento {
+  int evento;
+  char horario[20]; 
+  char data[20];
+  char compromisso[100];
 };
 
-int main() {
-    // Declaração de um array de 2 clientes    
-    struct Cliente clientes [2];
-    for (int i = 0; i < 2; i++){
-      printf("Digite o ID do cliente %d: ", i + 1);
-      scanf("%d", &clientes[i].id);
-      getchar(); // Limpa o buffer do teclado
+int main() { 
+    struct Evento eventos [5];
+    for (int i = 0; i < 5; i++){
+      printf("\nEvento %d: \n", i + 1);
 
-      printf("Digite o nome do cliente %d: ", i + 1);
-      fgets(clientes[i].nome, sizeof(clientes[i].nome), stdin);
-      clientes[i].nome [strcspn (clientes[i].nome, "\n")] = 0; // Remove o '\n' do final
+      printf("Digite o Horário do compromisso %d: ", i + 1);
+      fgets(eventos[i].horario, sizeof(eventos[i].horario), stdin);
+      eventos[i].horario [strcspn (eventos[i].horario, "\n")] = 0; // Remove o '\n' do final
 
-      printf("Digite o email do cliente %d: ", i + 1);
-      fgets(clientes[i].email, sizeof(clientes[i].email), stdin);
-      clientes[i].email[strcspn(clientes[i].email, "\n")] = 0; // Remove o '\n' do final
+      printf("Digite a Data %d: ", i + 1);
+      fgets(eventos[i].data, sizeof(eventos[i].data), stdin);
+      eventos[i].data[strcspn(eventos[i].data, "\n")] = 0; // Remove o '\n' do final
 
-      printf("Digite a idade do cliente %d: ", i + 1);
-      scanf("%d", &clientes[i].idade);
+      printf("Descreva o compromisso %d: ", i + 1);
+      fgets(eventos[i].compromisso, sizeof(eventos[i].compromisso), stdin);
+      eventos[i].compromisso [strcspn (eventos[i].compromisso, "\n")] = 0; // Remove o '\n' 
     }
-  printf("\nInformações dos clientes:\n");
-  for (int i = 0; i < 2; i++){
-    printf("Cliente %d:\n", i + 1);
-    printf("ID: %d\n", clientes[i].id);
-    printf("Nome: %s\n", clientes[i].nome);
-    printf("Email: %s\n", clientes[i].email);
-    printf("Idade: %d\n", clientes[i].idade);
+
+  printf("\nEventos salvos na Agenda:\n");
+  for (int i = 0; i < 5; i++){
+    printf("\nEvento %d:\n", i + 1);
+    printf("Horário: %s\n", eventos[i].horario);
+    printf("Data: %s\n", eventos[i].data);
+    printf("Compromisso: %s\n", eventos[i].compromisso);
   }
 }
