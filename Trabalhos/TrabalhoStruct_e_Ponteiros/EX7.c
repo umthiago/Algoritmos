d2e35d648276b2b66f107b29725d1cdc7fac7b4c
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-24 19:43:44 -0300

    Update EX7.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX7.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX7.c
@@ -1,34 +1,16 @@
 #include <stdio.h>
 
-struct dados{
-    char endereco[40];
-    int idade;
-    char nome[40];
-};
-
-int main(){
-    struct dados alunos[3];
-
-    for(int i = 0; i < 3; i++){
-        printf("\nNome: ");
-        scanf("%[^\n]", alunos[i].nome);
-        getchar();
-        
-        printf("\nIdade: ");
-        scanf("%d", &alunos[i].idade);
-        getchar();
-        
-        printf("\nEndereco: ");
-        scanf("%[^\n]", alunos[i].endereco);
-        getchar();
-    }
+int main() {
+    int var1 = 1, var2 = 2;
     
-    printf("\n- Os dados armazenados -\n");
-   
-    for(int i = 0; i < 3; i++){
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Endereco: %s\n", alunos[i].endereco);
+    int *pont1 = &var1; 
+    int *pont2 = &var2; 
+
+
+    if (var1 > var2) {
+        printf("Maior endereco: %p (ponteiro 1)\n", (void*)pont1);
+    } else {
+        printf("Maior endereco: %p (ponteiro 2)\n", (void*)pont2);
     }
 
     return 0;
