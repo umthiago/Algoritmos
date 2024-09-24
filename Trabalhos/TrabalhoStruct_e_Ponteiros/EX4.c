87e734557fd5f7ec1446b9078a48d44bae82c8c2
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-24 16:32:11 -0300

    Update EX4.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX4.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX4.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
 
 struct dados{
-    char endereco[40];
-    int idade;
     char nome[40];
+    int matricula;
+    double nota1, nota2, nota3;
 };
 
 int main(){
-    struct dados alunos[3];
-
-    for(int i = 0; i < 3; i++){
-        printf("\nNome: ");
-        scanf("%[^\n]", alunos[i].nome);
-        getchar();
-        
-        printf("\nIdade: ");
-        scanf("%d", &alunos[i].idade);
-        getchar();
-        
-        printf("\nEndereco: ");
-        scanf("%[^\n]", alunos[i].endereco);
-        getchar();
-    }
     
-    printf("\n- Os dados armazenados -\n");
-   
-    for(int i = 0; i < 3; i++){
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Endereco: %s\n", alunos[i].endereco);
+    double maiornotaprova1 = 0;
+    int aux, aux1, aux2;
+    double media, maiormedia = 0, menormedia = 10;
+    
+
+    struct dados aluno[5];
+
+    for(int i = 0; i < 2; i++){
+        printf("\nNome: ");
+        scanf("%[^\n]", aluno[i].nome);
+        getchar();
+        
+        printf("Número de matrícula: ");
+        scanf("%d", &aluno[i].matricula);
+        getchar();
+        
+        printf("Nota da Primeira Prova: ");
+        scanf("%lf", &aluno[i].nota1);
+        getchar();
+        
+        printf("Nota da Segunda Prova: ");
+        scanf("%lf", &aluno[i].nota2);
+        getchar();
+        
+        printf("Nota da Terceira Prova: ");
+        scanf("%lf", &aluno[i].nota3);
+        getchar();
     }
+
+    
+    
+    for(int i = 0; i < 5; i++){
+        if(aluno[i].nota1 > maiornotaprova1){
+            maiornotaprova1  = aluno[i].nota1;
+            aux = i;
+        }
+    }
+    printf("\n");
+
+    
+    for(int i = 0; i < 5; i++){
+        media = (aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3) / 3;
+        if(media >= 6){
+            printf("%s foi APROVADO com Média: %.1lf\n", aluno[i].nome, media);
+        } else{
+            printf("%s foi REPROVADO com Média: %.1lf\n", aluno[i].nome, media);
+        }
+
+        if(media > maiormedia){
+            maiormedia = media;
+            aux1 = i;
+        }
+        if(media < menormedia){
+            menormedia = media;
+            aux2 = i;
+        }
+    }
+
+    printf("\nO aluno com maior nota da primeira prova foi: %s com a nota de: %.1lf\n", aluno[aux].nome, maiornotaprova1);
+    printf("O aluno com maior média geral foi: %s com a média de: %.1lf\n", aluno[aux1].nome, maiormedia);
+    printf("O aluno com menor média geral foi: %s com a média %.1lf\n", aluno[aux2].nome, menormedia);
 
     return 0;
 }
