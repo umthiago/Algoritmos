3250da32812a5b7f382bfc1e5bde7febdfe754d5
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-24 20:02:13 -0300

    Update EX9.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX9.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX9.c
@@ -2,14 +2,15 @@
 
 int main(){
 
-    double vetor[10] = { 95.4, 43.8, 17.1, 11.4, 51.2, 22.7, 52.4, 77.6, 16.7, 33.8};
+    double matriz[3][3] = { 95.4, 43.8, 17.1, 11.4, 51.2, 22.7, 52.4, 77.6, 16.7};
     double *ponteiro;
     
-    for(int i = 0; i < 10; i++){
-        ponteiro = &vetor[i];
-        printf("Posicao %d: endereco %p\n", i , (ponteiro ));
-        
 
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            ponteiro = &matriz[i][j];
+            printf("Posicao [%d][%d]: endereco %p\n", i + 1, j + 1, (void*)(ponteiro));
+        }
     }
 
     return 0;
