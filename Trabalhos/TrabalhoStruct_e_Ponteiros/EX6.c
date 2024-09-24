9b4ce157e9df5eaffd97d1b16bec3afc870efd84
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-24 19:45:35 -0300

    Update EX6.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX6.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX6.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    int var1 = 1;
-    int var2 = 2;
-    int *endereco1, *endereco2;
+int main() {
+    int var1 = 1, var2 = 2;
+    
+    int *pont1 = &var1; 
+    int *pont2 = &var2; 
 
-    endereco1 = &var1;
-    endereco2 = &var2;
 
-    if((void*)endereco1 < (void*)endereco2){
-        printf("O maior endereço dos dois é o: %p \n", (void*)endereco1);
-    } else{
-        printf("O maior endereço dos dois é o: %p \n", (void*)endereco2);
+    if (var1 > var2) {
+        printf("Maior endereco: %p (ponteiro 1)\n", (void*)pont1);
+    } else {
+        printf("Maior endereco: %p (ponteiro 2)\n", (void*)pont2);
     }
 
     return 0;
