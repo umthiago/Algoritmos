a654d11523cf977353f99b7fe63f710aa393e598
Thiago <124899004+umthiago@users.noreply.github.com>
2024-09-24 16:43:57 -0300

    Update EX6.c

--- Trabalhos/TrabalhoStruct_e_Ponteiros/EX6.c
+++ Trabalhos/TrabalhoStruct_e_Ponteiros/EX6.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 
-struct dados{
-    char endereco[40];
-    int idade;
-    char nome[40];
-};
-
 int main(){
-    struct dados alunos[3];
+    int var1 = 1;
+    int var2 = 2;
+    int *endereco1, *endereco2;
 
-    for(int i = 0; i < 3; i++){
-        printf("\nNome: ");
-        scanf("%[^\n]", alunos[i].nome);
-        getchar();
-        
-        printf("\nIdade: ");
-        scanf("%d", &alunos[i].idade);
-        getchar();
-        
-        printf("\nEndereco: ");
-        scanf("%[^\n]", alunos[i].endereco);
-        getchar();
-    }
-    
-    printf("\n- Os dados armazenados -\n");
-   
-    for(int i = 0; i < 3; i++){
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Endereco: %s\n", alunos[i].endereco);
+    endereco1 = &var1;
+    endereco2 = &var2;
+
+    if((void*)endereco1 < (void*)endereco2){
+        printf("O maior endereço dos dois é o: %p \n", (void*)endereco1);
+    } else{
+        printf("O maior endereço dos dois é o: %p \n", (void*)endereco2);
     }
 
     return 0;
