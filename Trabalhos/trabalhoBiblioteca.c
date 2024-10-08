19e7cdb552b05722089dedc9c14f538de85aa431
pedro.mujica1 <miottopedro@gmail.com>
2024-10-07 21:57:31 -0300

    SAVING

--- Trabalhos/trabalhoBiblioteca.c
+++ Trabalhos/trabalhoBiblioteca.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <float.h>
 
-void flt_max_exp(){
+
+//Mostra o Valor Maxímo e mínimo de um float
+void MaxMinFloat(){
+    printf("VALORES MAX E MIN\n");
+    printf("O máximo valor de um número em ponto flutuante é:  = %.10e\n", FLT_MAX);
+    printf("O minimo valor de um número em ponto flutuante é:  %.10e\n", FLT_MIN);
     printf("\n");
-    printf("The maximum value of 10-exponent for float: %d\n", FLT_MAX_EXP);
-    //"Informam o maior valor inteiro para um expoente na base FLT_RADIX."//
+    
 }
-void flt_max_10_exp(){
-    printf("\n");
-    printf("The maximum value of 10-exponent for float: %d\n", FLT_MAX_10_EXP);
-    //Informam o maior valor inteiro para um expoente na base decimal.//
+//Mostra o valor da mantissa ou seja quantidade de digitos após a virgula
+void floatMant(){
+    printf("VALOR MANT\n");
+    printf("Número de digitos do float (Valor Mantissa) = %d\n", FLT_MANT_DIG);
+    printf("Número de digitos do Double (Valor Mantissa) = %d\n", DBL_MANT_DIG);
+    printf("Número de digitos do Long Double (Valor Mantissa) = %d\n", LDBL_MANT_DIG);
 }
 
-void flt_mant_dig(){
-    
-    printf("TESTE: %2f \n ",FLT_DIG);
-    //Informam o número de dígitos do número na base FLT_RADIX.//
+//Informa o modo de arredondamento para todos os valores do tipo float, double e long double. Pode ser alterado com as funções presentes na biblioteca <fenv.h>. Cada valor, representa um modo, sendo eles://
+/*-1: indeterminado
+    0: em direção a zero
+    1: para o valor mais próximo
+    2: em direção ao infinito positivo
+    3: em direção ao infinito negativo */
+void flt_round(){
+    int a = FLT_ROUNDS;
+    printf("MODO DE ARREDONDAMENTO PARA VALORES FLOAT: %d\n", a);
 }
-void flt_epsilon(){
-    float epsilon = FLT_EPSILON;
-    printf("\n");
-    printf("The smallest positive difference between 1 and the next representable number: %f\n", epsilon);
-    //Informam o dígito menos significativo representável.//
-}
+
 
 void flt_radix(){
     int radix = FLT_RADIX;
@@ -31,30 +37,37 @@ void flt_radix(){
     //Informa a base do expoente, sendo 2 (binário), 10 (decimal) ou 16 (hexadecimal).//
 }
 
-void flt_round(){
-    float a = 1.0;
-    printf( "Initial FLT_ROUND: %f  %d\n",a, FLT_ROUNDS );
-    //Informa o modo de arredondamento para todos os valores do tipo float, double e long double. Pode ser alterado com as funções presentes na biblioteca <fenv.h>. Cada valor, representa um modo, sendo eles://
-    //-1: indeterminado
-//0: em direção a zero
-//1: para o valor mais próximo
-//2: em direção ao infinito positivo
-//3: em direção ao infinito negativo//
-    
-}
-
-void maxFloat(){
-    printf("The maximum value of float = %.10e\n", FLT_MAX);
-    printf("The minimum value of float = %.10e\n", FLT_MIN);
-
-    printf("The number of digits in the number = %.10d\n", FLT_MANT_DIG);
+void flt_epsilon(){
+    float epsilon = FLT_EPSILON;
     printf("\n");
-    //Informam o maior valor finito de um ponto flutuante.//
+    printf("The smallest positive difference between 1 and the next representable number: %f\n", epsilon);
+    //Informam o dígito menos significativo representável.//
 }
+
+void flt_mant_dig(){
+    
+    printf("TESTE: %d \n ",FLT_DIG);
+    //Informam o número de dígitos do número na base FLT_RADIX.//
+}
+
+void flt_max_10_exp(){
+    printf("\n");
+    printf("The maximum value of 10-exponent for float: %d\n", FLT_MAX_10_EXP);
+    //Informam o maior valor inteiro para um expoente na base decimal.//
+}
+
+void flt_max_exp(){
+    printf("\n");
+    printf("The maximum value of 10-exponent for float: %d\n", FLT_MAX_EXP);
+    //"Informam o maior valor inteiro para um expoente na base FLT_RADIX."//
+}
+
+
 
 int main () {
     
-    maxFloat();
+    MaxMinFloat();
+    floatMant();
     flt_round();
     flt_radix();
     flt_epsilon();
