b2ce4648891113e01a4755d184e8307cdeacfdc9
Thiago <124899004+umthiago@users.noreply.github.com>
2025-01-28 16:41:10 -0300

    Update Ex1.c

--- Listas/ListaBubbleSort/Ex1.c
+++ Listas/ListaBubbleSort/Ex1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+void bubbleSort(int vetor[], int n){
+    int temp;
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if(vetor[j] > vetor[j+1]){
+                temp = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int vetor[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(vetor) / sizeof(vetor[0]);
+    bubbleSort(vetor, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+    return 0;
+}
