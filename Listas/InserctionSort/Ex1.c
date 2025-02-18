273523b3e32ba78647646f3ca644df8882f823aa
Thiago <124899004+umthiago@users.noreply.github.com>
2025-02-18 16:53:01 -0300

    Update Ex1.c

--- Listas/InserctionSort/Ex1.c
+++ Listas/InserctionSort/Ex1.c
@@ -13,17 +13,16 @@ void insertionSort(int vetor[], int n){
         vetor[j+1] = key; 
     } 
 }
-
 int main(void) {
-  int vetor[] = {5,4,3,2,1};
+  int vetor[5] = {};
+  for(int i = 0; i < 5; i++){
+    scanf("%d", &vetor[i]);
+  }
   int n = sizeof(vetor)/sizeof(vetor[0]);
   insertionSort(vetor, n);
   for(int i = 0; i < n; i++){
     printf("%d ", vetor[i]);
-    
   }
-  
   return 0;
-
 }
  
